0af1c7ff1ce460603bb2db420fd69f2dc8309b89
Aristotoei <thavornsan@outlook.com>
2024-10-18 19:01:10 +0700

    first prototype

---
+++ script.js
@@ -0,0 +1,25 @@
+const InputBox = document.getElementById("input-box");
+const ListContainer = document.getElementById("list-container");
+function addTask(){
+  if(InputBox.value === "") {
+    alert("Add your text!");
+  }
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = InputBox.value;
+    ListContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  InputBox.value = "";
+}
+
+ListContainer.addEventListener("click", function(task) {
+  if(task.target.tagName === "LI") {
+    task.target.classList.toggle("checked");
+  }
+  else if(task.target.tagName === "SPAN") {
+    task.target.parentElement.remove();
+  }
+});
